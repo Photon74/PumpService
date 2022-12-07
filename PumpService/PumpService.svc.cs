7105f791f6721561ab0f9430e184de16e9e35779
Alexandr Kolodinsky <Photon.74@gmail.com>
2022-12-07 21:26:39 +0300

    Добавьте файлы проекта.

---
+++ PumpService/PumpService.svc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PumpService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "PumpService" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы PumpService.svc или PumpService.svc.cs в обозревателе решений и начните отладку.
+    public class PumpService
+    {
+
+    }
+}
