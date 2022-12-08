7ce6a7388f3c088b9b2e00a6aa1fd3cb2cf4ac3d
Alexandr Kolodinsky <Photon.74@gmail.com>
2022-12-08 19:48:52 +0300

    Client is done

---
+++ PumpClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using PumpClient.PumpServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PumpClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new PumpServiceClient(new InstanceContext(new CallbackHandler()));
+
+            client.UpdateAndCompileScript(@"C:\Users\Photo\source\repos\PumpService\PumpService\Scripts\Sample.script");
+            client.RunScript();
+
+            Console.ReadLine();
+            client.Close();
+        }
+    }
+}
